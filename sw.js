fe2e1033afb42f7731dfdc78f40c4a26095e8a0a
oono198279-www <oono198279@gmail.com>
2026-02-15 23:22:20 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = 'mgmt-pad-v18';  // ←更新時は番号を上げる
+const CACHE_NAME = 'mgmt-pad-v19';  // ←更新時は番号を上げる
 const APP_SHELL = [
   './',                // ルート
   './index.html',
